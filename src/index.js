251991856f16095bfe7ce0e10e51265f875a51cc
scazorla19 <rey@SkyNet.local>
2020-04-23 15:03:20 +1000

    Initial upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'tachyons';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
+import {robots} from './robots';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <CardList robots={robots} />
+  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
